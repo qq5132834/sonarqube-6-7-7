03cfe3fd89648656ba0b1ce006baa3b42cfd2501
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-03-30 17:30:08 +0800

    提交读取客户端submitAction的zip中的pb文件的测试用例

--- sonar-scanner-protocol/src/test/java/org/sonar/scanner/protocol/output/ScannerReportReaderMetaDataTest.java
+++ sonar-scanner-protocol/src/test/java/org/sonar/scanner/protocol/output/ScannerReportReaderMetaDataTest.java
@@ -1,24 +1,68 @@
 package org.sonar.scanner.protocol.output;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.sonar.core.util.Protobuf;
+import org.sonar.core.util.CloseableIterator;
 
 import java.io.File;
 
 public class ScannerReportReaderMetaDataTest {
 
+    private ScannerReportReader scannerReportReader;
+
+    @Before
+    public void readReport(){
+        String currentPath = System.getProperty("user.dir");
+        String dir = currentPath + "\\src\\test\\java\\org\\sonar\\scanner\\protocol\\output" + File.separator + "db" + File.separator;
+        scannerReportReader = new ScannerReportReader(new File(dir));
+    }
+
     /***
      * 读取metadata.bp文件内容
-     * 测试ScannerReportReader.java中获取readMetadata方法：测试ScannerReportReader.readMetadata
-     * @throws Exception
      */
     @Test
     public void readMetadataTest() throws Exception {
-        String currentPath = System.getProperty("user.dir");
-        String metadataFilePath = currentPath + "\\src\\test\\java\\org\\sonar\\scanner\\protocol\\output" + File.separator + "db" + File.separator + "metadata.pb";
-        File file = new File(metadataFilePath);
-        ScannerReport.Metadata metadata = Protobuf.read(file, ScannerReport.Metadata.parser());
+        ScannerReport.Metadata metadata = scannerReportReader.readMetadata();
         System.out.println("");
     }
+
+    /***
+     *
+     */
+    @Test
+    public void readComponentTest(){
+        ScannerReport.Component component = scannerReportReader.readComponent(1);
+        System.out.println("");
+    }
+
+    @Test
+    public void readActiveRulesTest(){
+        CloseableIterator<ScannerReport.ActiveRule> activeRuleCloseableIterator = this.scannerReportReader.readActiveRules();
+        while (activeRuleCloseableIterator.hasNext()){
+            ScannerReport.ActiveRule activeRule = activeRuleCloseableIterator.next();
+            System.out.println(activeRule.getRuleRepository() + "/" + activeRule.getRuleKey() + "/" + activeRule.getSeverity().name());
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void readContextPropsTest(){
+        CloseableIterator<ScannerReport.ContextProperty> contextPropertyCloseableIterator = this.scannerReportReader.readContextProperties();
+        while (contextPropertyCloseableIterator.hasNext()){
+            ScannerReport.ContextProperty contextProperty = contextPropertyCloseableIterator.next();
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void readCoveragesTest(){
+        CloseableIterator<ScannerReport.LineCoverage> lineCoverageCloseableIterator = this.scannerReportReader.readComponentCoverage(2);
+        while (lineCoverageCloseableIterator.hasNext()){
+            ScannerReport.LineCoverage lineCoverage = lineCoverageCloseableIterator.next();
+        }
+        System.out.println();
+    }
+
+
 
 }
