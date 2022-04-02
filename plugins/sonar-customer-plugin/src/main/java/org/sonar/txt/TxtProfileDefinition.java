79df793fa53c10dcd7aec2b4e807d6f71301b262
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-04-02 15:35:19 +0800

    commit

--- plugins/sonar-customer-plugin/src/main/java/org/sonar/txt/TxtProfileDefinition.java
+++ plugins/sonar-customer-plugin/src/main/java/org/sonar/txt/TxtProfileDefinition.java
@@ -10,12 +10,27 @@ import org.sonar.api.utils.ValidationMessages;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.sonar.api.rules.RulePriority.*;
+
 public class TxtProfileDefinition extends ProfileDefinition {
 
     public static final String PROFILE = "txt";
 
     @Override
     public RulesProfile createProfile(ValidationMessages validation) {
+        //return this.getRulesProfile();
+        return this.getActiveRulesProfile();
+    }
+
+    //添加激活规则
+    private RulesProfile getActiveRulesProfile(){
+        RulesProfile profile = RulesProfile.create(PROFILE, TxtLanguage.KEY);
+        profile.activateRule(Rule.create(TxtContainRuleDefinition.TXT_REPOSITORY, TxtSensor.RULE_KEY), BLOCKER);
+        return profile;
+    }
+
+    //添加未激活规则
+    private RulesProfile getRulesProfile(){
         RulesProfile rulesProfile = RulesProfile.create();
         rulesProfile.setName(PROFILE);
         rulesProfile.setLanguage(TxtLanguage.KEY);
@@ -28,7 +43,6 @@ public class TxtProfileDefinition extends ProfileDefinition {
 
         Rule rule = Rule.create(TxtContainRuleDefinition.TXT_REPOSITORY, TxtSensor.RULE_KEY);
         //rule.setStatus("");
-
         List<ActiveRule> activeRuleList = new ArrayList<>();
         ActiveRule activeRule = new ActiveRule();
         activeRule.setRule(rule);
@@ -41,5 +55,13 @@ public class TxtProfileDefinition extends ProfileDefinition {
 //        profile.activateRule(Rule.create(XooRulesDefinition.XOO_REPOSITORY, HasTagSensor.RULE_KEY), RulePriority.MAJOR);
 //        profile.activateRule(Rule.create(XooRulesDefinition.XOO_REPOSITORY, OneIssuePerLineSensor.RULE_KEY), RulePriority.INFO);
 //        profile.activateRule(Rule.create(XooRulesDefinition.XOO_REPOSITORY, OneIssuePerFileSensor.RULE_KEY), RulePriority.CRITICAL);
+
+//    public RulesProfile createProfile(ValidationMessages validation) {
+//        RulesProfile profile = RulesProfile.create("FooLint Rules", FooLanguage.KEY);
+//        profile.activateRule(Rule.create(REPO_KEY, "ExampleRule1"), BLOCKER);
+//        profile.activateRule(Rule.create(REPO_KEY, "ExampleRule2"), MAJOR);
+//        profile.activateRule(Rule.create(REPO_KEY, "ExampleRule3"), CRITICAL);
+//        return profile;
+//    }
 
 }
