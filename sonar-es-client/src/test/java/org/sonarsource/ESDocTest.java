31357c45ad4c7d84d52b9f10cd7d155a3182e364
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-04-10 17:35:28 +0800

    commit

--- sonar-es-client/src/test/java/org/sonarsource/ESDocTest.java
+++ sonar-es-client/src/test/java/org/sonarsource/ESDocTest.java
@@ -8,8 +8,12 @@ import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
@@ -21,6 +25,8 @@ import org.sonar.es.MyEsClient;
 
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /***
@@ -36,6 +42,11 @@ public class ESDocTest {
         this.client = MyEsClient.getTransportClient();
     }
 
+
+    /***
+     * 增加
+     * @throws Exception
+     */
     @Test
     public void createDoc() throws Exception {
 
@@ -55,11 +66,53 @@ public class ESDocTest {
         //通过client执行添加
         IndexResponse indexResponse = this.client.index(indexRequest).get();
 
-        //
+        //输出
         System.out.println(indexResponse.getResult().toString());
 
+    }
+
+    /***
+     * 删除
+     * @throws Exception
+     */
+    @Test
+    public void deleteDoc() throws Exception{
+        //创建request对象封装数据
+        String docId = "1";
+        DeleteRequest deleteRequest = new DeleteRequest(ESIndexTest.index, ESIndexTest.type, docId);
+
+        //client执行
+        DeleteResponse response = this.client.delete(deleteRequest).get();
+
+        //
+        System.out.println(response.getResult().toString());
+    }
+
+    /***
+     * 修改
+     * @throws Exception
+     */
+    @Test
+    public void updateDoc() throws Exception{
+        //1.创建一个map，指定需要修改的内容
+        Map<String, Object> doc = new HashMap<String, Object>();
+        doc.put("name", "张大三");
+        String docId = "1";
+
+        //创建request对象，封装对象
+        UpdateRequest updateRequest = new UpdateRequest(ESIndexTest.index, ESIndexTest.type, docId);
+        updateRequest.doc(doc);
+
+        //通过client对象执行
+        UpdateResponse updateResponse = this.client.update(updateRequest).get();
+
+        //
+        System.out.println(updateResponse.getResult().toString());
 
     }
+
+
+
 
 
 
