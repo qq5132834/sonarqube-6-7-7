4f8303bc07ece92f5b65976a0c3358ea71102f41
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-04-10 13:16:46 +0800

    commit

--- sonar-es-client/src/main/java/org/sonar/es/index/MyProjectMeasuresIndex.java
+++ sonar-es-client/src/main/java/org/sonar/es/index/MyProjectMeasuresIndex.java
@@ -17,12 +17,19 @@ public class MyProjectMeasuresIndex {
         myProjectMeasuresIndex.search();
     }
 
+    /***
+     * .../api/components/search_projects项目查询中抽取es的查询功能和结果返回
+     * @throws UnknownHostException
+     */
     public void search() throws UnknownHostException {
         ProjectMeasuresIndex projectMeasuresIndex = new ProjectMeasuresIndex(MyEsClient.getEsClient(MyEsClient.getTransportClient()),
                                                             new AuthorizationTypeSupport(new UserSessionRule()),
                                                             System2.INSTANCE);
 
         ProjectMeasuresQuery query = new ProjectMeasuresQuery();
+        query.setQueryText("f9d5a50ca66a4d7c99e9801b1173bae5");
+        query.setSort("analysisDate");
+        query.setAsc(true);
         SearchIdResult<String> stringSearchIdResult = projectMeasuresIndex.search(query, new SearchOptions().setOffset(0).setLimit(50));
         System.out.println();
     }
