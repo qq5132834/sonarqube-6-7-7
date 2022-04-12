5eb9b29c4c8f9e4fc72bbcd18b07aa7ea4ff5a6e
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-04-12 23:35:11 +0800

    commit

--- sonar-scanner-cli/src/main/java/org/sonarsource/scanner/cli/Conf.java
+++ sonar-scanner-cli/src/main/java/org/sonarsource/scanner/cli/Conf.java
@@ -59,7 +59,6 @@ class Conf {
     result.putAll(System.getProperties());
     result.putAll(loadEnvironmentProperties());
     result.putAll(cli.properties());
-    result.putAll(this.getCustomProperties());
     result = resolve(result);
 
     // root project base directory must be present and be absolute
@@ -75,20 +74,6 @@ class Conf {
 
   private Properties loadEnvironmentProperties() {
     return Utils.loadEnvironmentProperties(env);
-  }
-
-  private Properties getCustomProperties(){
-    String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-    Properties properties = new Properties();
-    properties.put("sonar.projectName", uuid);
-    properties.put("sonar.projectKey", uuid);
-    properties.put("sonar.projectVersion", "1.0");
-    properties.put("sonar.sources",       "C:\\Users\\51328\\Desktop\\sonarqube-6.7.7\\sonarqube-6.7.7\\sonar-scanner-cli\\src\\test\\resources\\org\\sonarsource\\scanner\\cli\\pluginTest");
-    properties.put("sonar.java.binaries", "C:\\Users\\51328\\Desktop\\sonarqube-6.7.7\\sonarqube-6.7.7\\sonar-scanner-cli\\target\\classes");
-    //properties.put("sonar.host.url", "http://192.168.32.140:9000");
-    properties.put("sonar.host.url", "http://127.0.0.1:9000");
-    properties.put("sonar.sourceEncoding", "UTF-8");
-    return properties;
   }
 
   private Properties loadGlobalProperties() {
