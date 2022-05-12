c1f9e02d69a74e50267f948590c7137ea93a38c6
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-05-12 11:52:07 +0800

    sonarqube中search_projects接口，可靠性评级reliablility_rating统计

--- sonar-es-client/src/test/java/org/sonarsource/sonarqube/aggs/SonarSearchProjectQuery.java
+++ sonar-es-client/src/test/java/org/sonarsource/sonarqube/aggs/SonarSearchProjectQuery.java
@@ -1,31 +1,33 @@
 package org.sonarsource.sonarqube.aggs;
 
 import org.elasticsearch.action.search.SearchAction;
-import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
-import org.elasticsearch.search.aggregations.AggregationBuilder;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.filter.FilterAggregationBuilder;
+import org.elasticsearch.search.aggregations.*;
+import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
+import org.elasticsearch.search.aggregations.bucket.filter.Filter;
 import org.elasticsearch.search.aggregations.bucket.filters.FiltersAggregator;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.aggregations.bucket.filters.InternalFilters;
+import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
+import org.elasticsearch.search.aggregations.bucket.missing.Missing;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.sum.Sum;
 import org.junit.Before;
 import org.junit.Test;
 import org.sonar.es.MyEsClient;
-import org.sonar.server.es.IndexType;
 
 import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 import static org.elasticsearch.search.aggregations.AggregationBuilders.filters;
-import static org.sonar.server.measure.index.ProjectMeasuresIndexDefinition.*;
+import static org.sonar.server.measure.index.ProjectMeasuresIndexDefinition.FIELD_MEASURES;
 
 /***
  * sonarqube中search_projects接口，可靠性评级reliablility_rating统计
@@ -174,8 +176,92 @@ public class SonarSearchProjectQuery {
         searchRequestBuilder.addAggregation(abstractAggregationBuilder);
 
         SearchResponse searchResponse = searchRequestBuilder.get();
+
+        Aggregations aggregations = searchResponse.getAggregations();
+        if (aggregations != null) {
+            for (Aggregation facet : aggregations) {
+                processAggregation(facet);
+            }
+        }
+
         System.out.println();
 
+    }
+
+    private void processAggregation(Aggregation aggregation) {
+        System.out.println(aggregation.getClass().getName());
+        if (Missing.class.isAssignableFrom(aggregation.getClass())) {
+            processMissingAggregation((Missing) aggregation);
+        } else if (Terms.class.isAssignableFrom(aggregation.getClass())) {
+            processTermsAggregation((Terms) aggregation);
+        } else if (Filter.class.isAssignableFrom(aggregation.getClass())) {
+            processSubAggregations((Filter) aggregation);
+        } else if (HasAggregations.class.isAssignableFrom(aggregation.getClass())) {
+            processSubAggregations((HasAggregations) aggregation);
+        } else if (Histogram.class.isAssignableFrom(aggregation.getClass())) {
+            processDateHistogram((Histogram) aggregation);
+        } else if (Sum.class.isAssignableFrom(aggregation.getClass())) {
+            processSum((Sum) aggregation);
+        } else if (MultiBucketsAggregation.class.isAssignableFrom(aggregation.getClass())) {
+            processMultiBucketAggregation((MultiBucketsAggregation) aggregation);
+        } else {
+            throw new IllegalArgumentException("Aggregation type not supported yet: " + aggregation.getClass());
+        }
+    }
+
+    private void processSum(Sum aggregation) {
+    }
+
+    private void processMissingAggregation(Missing aggregation ) {
+        long docCount = aggregation.getDocCount();
+        System.out.println(docCount);
+        if (docCount > 0L) {
+//            LinkedHashMap<String, Long> facet = getOrCreateFacet(aggregation.getName().replace("_missing", ""));
+//            if (aggregation.getAggregations().getAsMap().containsKey(FACET_MODE_EFFORT)) {
+//                facet.put("", Math.round(((Sum) aggregation.getAggregations().get(FACET_MODE_EFFORT)).getValue()));
+//            } else {
+//                facet.put("", docCount);
+//            }
+        }
+    }
+
+    private void processTermsAggregation(Terms aggregation) {
+        String facetName = aggregation.getName();
+        System.out.println(facetName);
+        for (Terms.Bucket value : aggregation.getBuckets()) {
+            System.out.println(value.getKeyAsString());
+            System.out.println(value.getDocCount());
+        }
+    }
+
+    private void processSubAggregations(HasAggregations aggregation) {
+        if (Filter.class.isAssignableFrom(aggregation.getClass())) {
+            Filter filter = (Filter) aggregation;
+            System.out.println(filter.getName());
+            System.out.println(filter.getDocCount());
+        }
+
+        for (Aggregation sub : aggregation.getAggregations()) {
+            processAggregation(sub);
+        }
+    }
+
+    private void processDateHistogram(Histogram aggregation) {
+        for (Histogram.Bucket value : aggregation.getBuckets()) {
+            System.out.println(value.getKeyAsString() + "," + value.getDocCount());
+        }
+    }
+
+    private void processMultiBucketAggregation(MultiBucketsAggregation aggregation) {
+        List list = aggregation.getBuckets();
+        for(int i= 0; i < list.size(); i++){
+            Object object = list.get(i);
+            System.out.println(object.getClass().getName());
+            if(object instanceof InternalFilters.InternalBucket){
+                InternalFilters.InternalBucket internalBucket = (InternalFilters.InternalBucket) object;
+                System.out.println(internalBucket.getKey() + "," + internalBucket.getDocCount());
+            }
+        }
     }
 
 }
