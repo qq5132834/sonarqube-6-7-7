f9aacad294a0e6e33bd6ba63da027bbebebbc059
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-05-14 21:06:38 +0800

    添加sca_rating、sca数据查询。

---
+++ sonar-java-native-access/src/main/java/org/sonarqube/jna/DeleteFile.java
@@ -0,0 +1,9 @@
+package org.sonarqube.jna;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public interface DeleteFile extends Library {
+    DeleteFile deleteFile = Native.load("Kernel32", DeleteFile.class); //调用Kernel32.dll 执行文件删除
+    boolean DeleteFileA(String lpFileName);
+}
