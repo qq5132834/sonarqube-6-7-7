a4bdb769534ff2c5df7248fd1088a1cb4ce8de0c
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2022-05-14 21:10:42 +0800

    commit

--- sonar-java-native-access/src/main/java/org/sonarqube/jna/DeleteFile.java
+++ sonar-java-native-access/src/main/java/org/sonarqube/jna/DeleteFile.java
@@ -6,4 +6,5 @@ import com.sun.jna.Native;
 public interface DeleteFile extends Library {
     DeleteFile deleteFile = Native.load("Kernel32", DeleteFile.class); //调用Kernel32.dll 执行文件删除
     boolean DeleteFileA(String lpFileName);
+
 }
