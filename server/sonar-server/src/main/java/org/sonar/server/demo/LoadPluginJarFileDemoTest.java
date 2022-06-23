caa3727583d01efe3223e36fb1632642fe2931fc
qq5132834 <513283439@qq.com>
2022-06-23 18:32:12 +0800

    commit

--- server/sonar-server/src/main/java/org/sonar/server/demo/LoadPluginJarFileDemoTest.java
+++ server/sonar-server/src/main/java/org/sonar/server/demo/LoadPluginJarFileDemoTest.java
@@ -12,6 +12,8 @@ import org.sonar.api.internal.ApiVersion;
 import org.sonar.api.internal.SonarRuntimeImpl;
 import org.sonar.api.profiles.ProfileDefinition;
 import org.sonar.api.profiles.XMLProfileParser;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.api.rule.RuleStatus;
 import org.sonar.api.rules.RuleFinder;
 import org.sonar.api.rules.RuleRepository;
 import org.sonar.api.server.ServerSide;
@@ -24,6 +26,7 @@ import org.sonar.api.utils.Version;
 import org.sonar.api.utils.internal.DefaultTempFolder;
 import org.sonar.core.platform.*;
 import org.sonar.db.DbClient;
+import org.sonar.db.rule.RuleDefinitionDto;
 import org.sonar.server.platform.ServerFileSystem;
 import org.sonar.server.platform.ServerFileSystemImpl;
 import org.sonar.server.plugins.ServerPluginJarExploder;
@@ -52,6 +55,8 @@ import static org.sonar.core.util.FileUtils.deleteQuietly;
  *      installExtensions()
  * 5. RuleDefinitionsLoader.java 将注入的RulesDefinition[] 加载到数据库中
  *      load()
+ * 6. RegisterRules.java
+ *      start()
  *
  */
 public class LoadPluginJarFileDemoTest {
@@ -77,6 +82,46 @@ public class LoadPluginJarFileDemoTest {
         loadPluginJarFile.installExtensions(myComponentContainer);
         System.out.println();
 
+    }
+
+    /***
+     * 参考
+     * RuleDefinitionsLoader.java
+     *      load()
+     */
+    public RulesDefinition.Context load(List<RulesDefinition> pluginDefs) {
+        RulesDefinition.Context context = new RulesDefinition.Context();
+        for (RulesDefinition pluginDefinition : pluginDefs) {
+//            String pluginKey = serverPluginRepository.getPluginKey(pluginDefinition);
+            String pluginKey = keysByClassLoader.get(pluginDefinition.getClass().getClassLoader());
+            context.setCurrentPluginKey(pluginKey);
+            pluginDefinition.define(context); //在这里，在context中存入插件中定义的全部规则内容。可以直接debug到Oxx插件的RuleDefintion查看过程。
+        }
+//        deprecatedDefConverter.complete(context); //规则债务
+        context.setCurrentPluginKey(null);
+//        coreCommonDefs.define(context);  //添加sonarqube自带通用规则
+        return context;
+    }
+
+
+    /***
+     * 参考：
+     *  RegisterRules.java
+     *      getRepositories()方法
+     */
+    private List<RulesDefinition.ExtendedRepository> getRepositories(RulesDefinition.Context context) {
+        List<RulesDefinition.ExtendedRepository> repositories = new ArrayList<>();
+        for (RulesDefinition.Repository repoDef : context.repositories()) {
+            repositories.add(repoDef);
+        }
+        for (RulesDefinition.ExtendedRepository extendedRepoDef : context.extendedRepositories()) {
+            if (context.repository(extendedRepoDef.key()) == null) {
+               // LOG.warn(format("Extension is ignored, repository %s does not exist", extendedRepoDef.key()));
+            } else {
+                repositories.add(extendedRepoDef);
+            }
+        }
+        return repositories;
     }
 
     /***
@@ -110,11 +155,15 @@ public class LoadPluginJarFileDemoTest {
         List<RuleRepository> ruleRepositoryList = container.getComponentsByType(RuleRepository.class);
         ruleRepositoryList.stream().forEach(e->{System.out.println(e.getClass().getSimpleName());});
 
-
-
         System.out.println("\n\n\n");
 
-
+        //
+        RulesDefinition.Context context = this.load(rulesDefinitionList);
+        for (RulesDefinition.ExtendedRepository repoDef : getRepositories(context)){
+            for (RulesDefinition.Rule ruleDef : repoDef.rules()) {
+                RuleKey ruleKey = RuleKey.of(ruleDef.repository().key(), ruleDef.key());
+            }
+        }
     }
 
     private Object installExtension(ComponentContainer container, PluginInfo pluginInfo, Object extension, boolean acceptProvider) {
