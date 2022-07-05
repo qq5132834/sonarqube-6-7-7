5041907222f9a98ec1e8623551d64ebdfc5ea9b7
qq5132834 <513283439@qq.com>
2022-07-05 10:49:26 +0800

    commit

--- springboot/src/main/java/com/sonar/huangl/SpringBootUnionApplication.java
+++ springboot/src/main/java/com/sonar/huangl/SpringBootUnionApplication.java
@@ -3,10 +3,6 @@ package com.sonar.huangl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/**
- * @author JING
- */
 @SpringBootApplication
 public class SpringBootUnionApplication {
     public static void main(String[] args) {
