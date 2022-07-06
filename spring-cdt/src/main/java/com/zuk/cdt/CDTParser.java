1b0be2d1161c45f95c819ed9c5f4d11c6b945489
qq5132834 <513283439@qq.com>
2022-07-06 17:20:52 +0800

    commit

--- spring-cdt/src/main/java/com/zuk/cdt/CDTParser.java
+++ spring-cdt/src/main/java/com/zuk/cdt/CDTParser.java
@@ -54,16 +54,41 @@ public class CDTParser {
             ImmutableMap<String, String> MACROS;
             {
                 ImmutableMap.Builder<String, String> macrosBuilder = new ImmutableMap.Builder();
+
+                // _Static_assert(cond, msg) feature of C11
                 macrosBuilder.put("_Static_assert(c, m)", "");
+
+                // These built-ins are defined as macros
+                // in
+                // org.eclipse.cdt.core.dom.parser.GNUScannerExtensionConfiguration.
+                // When the parser encounters their redefinition or
+                // some non-trivial usage in the code, we get parsing errors.
+                // So we redefine these macros to themselves in order to
+                // parse them as funtions.
                 macrosBuilder.put("__builtin_constant_p", "__builtin_constant_p");
                 macrosBuilder.put("__builtin_types_compatible_p(t1,t2)", "__builtin_types_compatible_p(({t1 arg1; arg1;}), ({t2 arg2; arg2;}))");
                 macrosBuilder.put("__offsetof__", "__offsetof__");
                 macrosBuilder.put("__func__", "\"__func__\"");
                 macrosBuilder.put("__FUNCTION__", "\"__FUNCTION__\"");
                 macrosBuilder.put("__PRETTY_FUNCTION__", "\"__PRETTY_FUNCTION__\"");
+
+                // Eclipse CDT 8.1.1 has problems with more complex attributes
                 macrosBuilder.put("__attribute__(a)", "");
+
+                // There are some interesting macros available at
+                // http://research.microsoft.com/en-us/um/redmond/projects/invisible/include/stdarg.h.htm
                 macrosBuilder.put("_INTSIZEOF(n)", "((sizeof(n) + sizeof(int) - 1) & ~(sizeof(int) - 1))"); // at
+                // least
+                // size
+                // of
+                // smallest
+                // addressable
+                // unit
+                // macrosBuilder.put("__builtin_va_start(ap,v)",
+                // "(ap = (va_list)&v + _INTSIZEOF(v))");
                 macrosBuilder.put("__builtin_va_arg(ap,t)", "*(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t))");
+
+                // macrosBuilder.put("__builtin_va_end(ap)", "ap=(va_list)0");
                 MACROS = macrosBuilder.build();
             }
 
