243d08038cdca9332bf04ecb9c6a8b920951ed2b
qq5132834 <513283439@qq.com>
2022-07-07 10:49:05 +0800

    commit

--- spring-cdt/src/main/java/com/zuk/cdt/Main.java
+++ spring-cdt/src/main/java/com/zuk/cdt/Main.java
@@ -1,10 +1,12 @@
 package com.zuk.cdt;
 
+import org.eclipse.cdt.core.CCorePlugin;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.cdt.core.model.CoreModel;
-import org.eclipse.cdt.core.model.ITranslationUnit;
-import org.eclipse.core.resources.IFile;
+import org.eclipse.cdt.core.model.ICProject;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -26,15 +28,21 @@ public class Main {
             System.out.println(funName + "," + startLine + "," + endLine);
         });
 
+        /**
+         * 1. org.eclipse.core.resources.xxxxx.jar
+         *
+         * */
+//        IPath path = new Path(file);
+//        IWorkspace iWorkspace = ResourcesPlugin.getWorkspace();
+//        IFile iFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+//        ITranslationUnit tu = (ITranslationUnit) CoreModel.getDefault().create(iFile);
+//        IASTTranslationUnit ast = tu.getAST();
         //
-        IPath path = new Path(file);
-        IFile iFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
-        ITranslationUnit tu = (ITranslationUnit) CoreModel.getDefault().create(iFile);
-        IASTTranslationUnit ast = tu.getAST();
-        //
-
-
-
+        ICProject project = CoreModel.getDefault().getCModel().getCProject("C:\\Users\\51328\\Desktop\\sonarqube-6.7.7\\sonarqube-6.7.7\\spring-cdt\\src\\main\\resources");
+        IIndex index =CCorePlugin.getIndexManager().getIndex(project);
+//
+//
+//        org.eclipse.core.runtime.Plugin a;
 
         System.out.println("");
     }
