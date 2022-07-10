77a0b6fe80c62fe3d201789b7ed6b62beb93390b
qq5132834 <513283439@qq.com>
2022-07-10 13:16:10 +0800

    commit

--- spring_eclipse/cdt/src/main/java/com/zuk/eclipse/TestMain.java
+++ spring_eclipse/cdt/src/main/java/com/zuk/eclipse/TestMain.java
@@ -1,7 +1,7 @@
 package com.zuk.eclipse;
-import org.eclipse.core.launcher.Main;
+
 public class TestMain {
     public static void main(String[] args) {
-        Main.main(args);
+        org.eclipse.equinox.launcher.Main.main(args);
     }
 }
--- spring_eclipse/src/main/java/com/zuk/eclipse/TestMain.java
+++
@@ -1,4 +0,0 @@
-package com.zuk.eclipse;
-
-public class Main {
-}
