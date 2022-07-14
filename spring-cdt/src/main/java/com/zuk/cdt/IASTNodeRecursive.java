6ba8925fbf5bd7bf783da35035b0988aaba9815a
qq5132834 <513283439@qq.com>
2022-07-14 11:40:14 +0800

    commit

--- spring-cdt/src/main/java/com/zuk/cdt/IASTNodeRecursive.java
+++ spring-cdt/src/main/java/com/zuk/cdt/IASTNodeRecursive.java
@@ -5,34 +5,31 @@ import com.zuk.cdt.file.var.FileVariableUtil;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Consumer;
 
 public class IASTNodeRecursive {
-
-    private static Set<String> IBINDING_SET = new HashSet<>();
 
     /***
      * 遍历AST
      */
     public static void recur(IASTNode iastNode){
+
         //输出节点信息
-        doIASTNode(iastNode, IASTNodeRecursive::printIASTNode);
-        //获取节点IBinding信息
+        //doIASTNode(iastNode, IASTNodeRecursive::printIASTNode);
+
+        //获取方法中入参、变量信息
         doIASTNode(iastNode, FileVariableUtil::methodParams);
-        //获取函数与函数调用信息
+
+        //获取方法中函数调用信息
         doIASTNode(iastNode, MethodCallIBinding::funcationCall);
 
-        IASTNode[] iastNodes = iastNode.getChildren();
-        if(iastNodes != null && iastNodes.length > 0){
-            //递归
-            Arrays.stream(iastNodes).forEach(IASTNodeRecursive::recur);
-        }
+        //递归遍历IASTNode的子节点
+        Arrays.stream(iastNode.getChildren()).forEach(IASTNodeRecursive::recur);
+
     }
 
     private static void printIASTNode(IASTNode iastNode){
-        //System.out.println(iastNode.getFileLocation().getStartingLineNumber() + "\n" + iastNode.getRawSignature());
+        System.out.println(iastNode.getFileLocation().getStartingLineNumber() + "\n" + iastNode.getRawSignature());
     }
 
     private static void doIASTNode(IASTNode iastNode, Consumer<IASTNode> consumer){
